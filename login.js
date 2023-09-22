1ef8c7beddab944cd14902c24723e9d89968dfac
Bealus <bealus.bs@yahoo.com>
2023-09-21 20:12:46 -0500

    change1

--- login.js
+++ login.js
@@ -1,4 +1,6 @@
 // login.js
+
+
 function simulateLogin() {
     const username = document.getElementById('login-username').value;
     const password = document.getElementById('login-password').value;
@@ -8,10 +10,15 @@ function simulateLogin() {
     if (registeredUsers[username] === password) {
         // Store the logged-in user's information in localStorage
         localStorage.setItem('loggedInUser', username);
+        userProfile = userProfiles[username];
 
-        // Redirect to the homepage
-        window.location.href = 'homepage.html';
-
+        // Check if the user's profile is incomplete
+        if(!userProfile.fullname) {
+            window.location.href = 'complete-profile.html'; //Send them to complete profile.
+        } else {
+            window.location.href = 'Fuel-Quote-Form.html'; //Send them to fuel quote.
+        }
+       
         return false; // Prevent the form from submitting
     } else {
         message.innerText = 'Invalid username or password';
