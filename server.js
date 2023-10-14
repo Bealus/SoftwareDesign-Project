a2c91088364bfdebc8a06cea14066295b9a6a91a
Bealus <64119426+Bealus@users.noreply.github.com>
2023-10-13 19:37:55 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,45 +8,51 @@ app.use(express.json());
 app.use(express.static('public')); // 'public' is the directory where your static files (including CSS) are located
 
 // In-memory storage for registered users (You should use a database in a real application)
-const users = [];
-// users - add other details on push register, bool compltedProfile = false;, database different
+const users = [
+    {
+        username: 'user1',
+        password: 'password1',
+        isProfileCompleted: false,
+    },
+    {
+        username: 'user2',
+        password: 'password2',
+        isProfileCompleted: true,
+    },
+];
+
 app.get('/register', (req, res) => {
-    //res.sendFile(__dirname + '/register.html');
-    res.sendFile(path.join(__dirname, '/public/register.html'));
+    res.sendFile(path.join(__dirname, '/public/register.html')); //Sends 200 status code by default.
 });
 
 app.post('/register', (req, res) => {
     const { username, password } = req.body;
-    //Test if data is being sent from front-end to back-end
-    //console.log(username); // Log the username
-    
-    if (!username || !password ) {
+    //Missing username or password
+    if (!username || !password) {
         res.status(400).send('All fields are required');
-      } else if (password.length < 6) {
+    } else if (password.length < 6) {
         res.status(400).send('Password must be at least 6 characters long');
-      }else if (users.some(user => user.username === username)) {
+    } else if (users.some(user => user.username === username)) {
         res.status(400).send('Username already taken. Please choose another one.');
     } else {
+        //Send data to database
         const newUser = {
             username,
             password,
             isProfileCompleted: false,
         };
-        //console.log(newUser); // Log the username
+        
         // Save the user in memory (In a real application, you would use a database)
         users.push(newUser);
-
-        // Redirect to a login page or another page
-       res.redirect('/login');
         
-        
-        //res.status(200).sendFile(path.join(__dirname, 'login.html'));
+        //res.redirect('/login');
+        res.status(200).send('Registration successful');
+    
     }
     
 });
 
 app.get('/login', (req, res) => {
-    //res.sendFile(__dirname + '/login.html');
     res.sendFile(path.join(__dirname, '/public/login.html'));
 });
 
@@ -66,7 +72,7 @@ app.post('/login', (req, res) => {
             res.redirect('/profile'); 
         }
     } else {
-        res.send('Invalid username or password');
+        res.status(401).send('Invalid username or password');
     }
 });
 
@@ -124,3 +130,4 @@ app.get('/history', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+module.exports = { app, users };
